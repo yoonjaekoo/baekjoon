22a20f679c1747517e8a952fb1c81be3aac0dc32
yoonjaekoo <cstdio.code@gmail.com>
2026-02-07 18:51:40 +0900

    9095 풀음

---
+++ 9095.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int nn;
+    cin>>nn;
+    int dp[100]={0};
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=4;
+    for(int i=4;i<=10;i++){
+        dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+    }
+    for(int i=0;i<nn;i++){
+        int n;
+        cin>>n;
+        cout<<dp[i]<<"\n";
+    }
+}
