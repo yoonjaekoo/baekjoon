7c3d6bac95a3d8d79f1b9055728c3e42da07c732
yoonjaekoo <cstdio.code@gmail.com>
2026-02-10 21:03:03 +0900

    14495 풀음

---
+++ 14495.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int n;
+    vector<int> fibonaccibismurihansuyeol(n+1);
+    fibonaccibismurihansuyeol[0]=1;
+    fibonaccibismurihansuyeol[1]=1;
+    fibonaccibismurihansuyeol[2]=1;
+
+    for(int i=3;i<n;i++){
+        fibonaccibismurihansuyeol[i]=fibonaccibismurihansuyeol[i-1]+fibonaccibismurihansuyeol[i-3];
+    }
+    cout<<fibonaccibismurihansuyeol[n-1];
+}
