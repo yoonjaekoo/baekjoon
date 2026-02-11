5a487e50f29e0ecc21932a377c9cfe57abe11ae8
yoonjaekoo <cstdio.code@gmail.com>
2026-02-11 16:47:39 +0900

    30501 어이쿠 대문자를 빼트렸네

--- 30501.cpp
+++ 30501.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
         string s;
         cin>>s;
         for(int j=0;j<s.length();j++){
-            if(s[j] == 's'){
+            if(s[j] == 's' || s[j] == 'S'){
                 cout<<s;
                 return 0;
             }
